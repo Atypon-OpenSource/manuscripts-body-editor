6978a3ccea2a2fd63516febcdcf1fdc03ac1c10a
Alf Eaton <aeaton@atypon.com>
2019-03-06 11:09:33 +0000

    Add a special case for DOI searches

--- src/lib/crossref.ts
+++ src/lib/crossref.ts
@@ -17,34 +17,79 @@
 import { stringify } from 'qs'
 import { convertDataToBibliographyItem } from '../csl'
 
-const search = (query: string, rows: number, mailto: string) =>
-  window
-    .fetch(
-      'https://api.crossref.org/works?' +
-        stringify({
-          filter: 'type:journal-article',
-          query,
-          rows,
-          mailto,
-        })
-    )
-    .then(response => response.json())
-    .then(({ message: { items, 'total-results': total } }) => ({
-      items: items.map(convertDataToBibliographyItem),
-      total,
-    }))
+const search = async (query: string, rows: number, mailto: string) => {
+  // if the query is just a DOI, fetch that single record
+  if (query.trim().match(/^10\.\S+\/\S+$/)) {
+    return searchByDOI(query.trim(), mailto)
+  }
 
-const fetch = (doi: string, mailto: string) =>
-  window
-    .fetch(
-      `https://api.crossref.org/works/${encodeURIComponent(
-        doi
-      )}/transform/application/vnd.citationstyles.csl+json?` +
-        stringify({
-          mailto,
-        })
-    )
-    .then(response => response.json())
-    .then(convertDataToBibliographyItem)
+  const response = await window.fetch(
+    'https://api.crossref.org/works?' +
+      stringify({
+        filter: 'type:journal-article',
+        query,
+        rows,
+        mailto,
+      })
+  )
+
+  if (!response.ok) {
+    throw new Error('There was a problem searching for this query.')
+  }
+
+  const {
+    message: { items, 'total-results': total },
+  } = await response.json()
+
+  return {
+    items: items.map(convertDataToBibliographyItem),
+    total,
+  }
+}
+
+const searchByDOI = async (doi: string, mailto: string) => {
+  const response = await window.fetch(
+    `https://api.crossref.org/works/${encodeURIComponent(doi)}?` +
+      stringify({
+        mailto,
+      })
+  )
+
+  if (!response.ok) {
+    if (response.status === 404) {
+      throw new Error('An item with this DOI could not be found.')
+    }
+    throw new Error('There was a problem searching for this DOI.')
+  }
+
+  const { message: item } = await response.json()
+
+  return {
+    items: [item],
+    'total-results': 1,
+  }
+}
+
+const fetch = async (doi: string, mailto: string) => {
+  const response = await window.fetch(
+    `https://api.crossref.org/works/${encodeURIComponent(
+      doi
+    )}/transform/application/vnd.citationstyles.csl+json?` +
+      stringify({
+        mailto,
+      })
+  )
+
+  if (!response.ok) {
+    if (response.status === 404) {
+      throw new Error('An item with this DOI could not be found.')
+    }
+    throw new Error('There was a problem fetching this DOI.')
+  }
+
+  const data = await response.json()
+
+  return convertDataToBibliographyItem(data)
+}
 
 export const crossref = { fetch, search }
