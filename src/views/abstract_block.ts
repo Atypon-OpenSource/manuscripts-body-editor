672d86c0f816698ac867aeffd08baff6bc35b844
Alf Eaton <aeaton@atypon.com>
2019-03-25 20:09:21 +0000

    Use PNG data URIs for gutter icons

--- src/views/abstract_block.ts
+++ src/views/abstract_block.ts
@@ -38,17 +38,17 @@ abstract class AbstractBlock implements NodeView {
   protected readonly props: EditorProps
   protected readonly getPos: () => number
   protected node: ManuscriptNode
-  protected readonly icons = {
-    plus:
-      '<svg width="16" height="16" stroke="currentColor"><line x1="8" y1="3" x2="8" y2="13"/><line x1="3" y1="8" x2="13" y2="8"/></svg>',
-    circle:
-      '<svg width="16" height="16" stroke="currentColor"><circle r="4" cx="8" cy="8"/></svg>',
-    attention: `<svg width="24" height="24"><g fill="none" fill-rule="evenodd">
-      <circle fill="#E28327" cx="12" cy="18.7" r="1"></circle>
-      <rect fill="#E28327" x="11.12" y="7.5" width="1.8" height="9" rx="0.9"></rect>
-      <path d="M12.901 1.98l9.41 19.587a1 1 0 0 1-.9 1.433H2.59a1 1 0 0 1-.901-1.433l9.41-19.586a1 1 0 0 1 1.802 0z" stroke="#E28327" stroke-width="1.5"></path>
-    </g></svg>`,
-  }
+  // protected readonly icons = {
+  //   plus:
+  //     '<svg width="16" height="16" stroke="currentColor"><line x1="8" y1="3" x2="8" y2="13"/><line x1="3" y1="8" x2="13" y2="8"/></svg>',
+  //   circle:
+  //     '<svg width="16" height="16" stroke="currentColor"><circle r="4" cx="8" cy="8"/></svg>',
+  //   attention: `<svg width="24" height="24"><g fill="none" fill-rule="evenodd">
+  //     <circle fill="#E28327" cx="12" cy="18.7" r="1"></circle>
+  //     <rect fill="#E28327" x="11.12" y="7.5" width="1.8" height="9" rx="0.9"></rect>
+  //     <path d="M12.901 1.98l9.41 19.587a1 1 0 0 1-.9 1.433H2.59a1 1 0 0 1-.901-1.433l9.41-19.586a1 1 0 0 1 1.802 0z" stroke="#E28327" stroke-width="1.5"></path>
+  //   </g></svg>`,
+  // }
   protected readonly view: ManuscriptEditorView
 
   protected constructor(
@@ -149,8 +149,8 @@ abstract class AbstractBlock implements NodeView {
   private createSyncWarningButton = () => {
     const warningButton = document.createElement('button')
     warningButton.classList.add('action-button')
-    warningButton.classList.add('has-sync-error')
-    warningButton.innerHTML = this.icons.attention
+    warningButton.classList.add('sync-warning-button')
+    // warningButton.innerHTML = this.icons.attention
 
     warningButton.addEventListener('click', async () => {
       const errors = this.syncErrors.map(error => error._id)
@@ -205,7 +205,7 @@ abstract class AbstractBlock implements NodeView {
     button.classList.add('add-block')
     button.classList.add(after ? 'add-block-after' : 'add-block-before')
     button.title = 'Add a new block'
-    button.innerHTML = this.icons.plus
+    // button.innerHTML = this.icons.plus
     button.addEventListener('mousedown', event => {
       event.preventDefault()
       event.stopPropagation()
@@ -221,7 +221,7 @@ abstract class AbstractBlock implements NodeView {
     const button = document.createElement('a')
     button.classList.add('edit-block')
     button.title = 'Edit block'
-    button.innerHTML = this.icons.circle
+    // button.innerHTML = this.icons.circle
     button.addEventListener('mousedown', event => {
       event.preventDefault()
       event.stopPropagation()
