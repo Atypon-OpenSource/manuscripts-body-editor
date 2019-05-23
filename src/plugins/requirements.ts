71b85a264e7736ef95e4c6225858a3b1b2598391
Alf Eaton <eaton.alf@gmail.com>
2019-05-23 10:42:22 +0100

    WIP

--- src/plugins/requirements.ts
+++ src/plugins/requirements.ts
@@ -1,7 +1,29 @@
-import { Manuscript, Model } from '@manuscripts/manuscripts-json-schema'
+/*!
+ * © 2019 Atypon Systems LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+  DEFAULT_BUNDLE,
+  ManuscriptSchema,
+} from '@manuscripts/manuscript-transform'
+import {
+  Manuscript,
+  Model,
+  Requirement,
+} from '@manuscripts/manuscripts-json-schema'
 import { Plugin } from 'prosemirror-state'
-import { DEFAULT_BUNDLE, ManuscriptSchema } from '..'
-// import { Decoration, DecorationSet } from 'prosemirror-view'
 
 interface ManuscriptTemplate extends Model {
   parent?: string
@@ -53,8 +75,14 @@ const buildTemplate = (
   return template
 }
 
+interface PluginState {
+  template?: ManuscriptTemplate
+  bundle?: Model
+  requirements?: Requirement[]
+}
+
 export default (props: Props) =>
-  new Plugin<ManuscriptSchema>({
+  new Plugin<PluginState, ManuscriptSchema>({
     state: {
       init() {
         const { modelMap, getManuscript } = props
@@ -62,7 +90,7 @@ export default (props: Props) =>
         const template = buildTemplate(modelMap)
 
         if (!template) {
-          return
+          return {}
         }
 
         const manuscript = getManuscript()
@@ -73,7 +101,7 @@ export default (props: Props) =>
 
         const requirements = (template.requirementIDs || []).map(id =>
           modelMap.get(id)
-        )
+        ) as Requirement[]
 
         return {
           template,
