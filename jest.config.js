812b7589b9e14664c34a2b02c5ce45fd5a15d9a1
Alf Eaton <eaton.alf@gmail.com>
2019-11-21 14:10:04 +0000

    Fix tests

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,12 @@ module.exports = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
   moduleNameMapper: {
     '^.+\\.css$': '<rootDir>/src/__mocks__/styleMock.ts',
+    // https://react-dnd.github.io/react-dnd/docs/testing#setup
+    "^dnd-core$": "dnd-core/dist/cjs",
+    "^react-dnd$": "react-dnd/dist/cjs",
+    // map react-dnd-html5-backend to react-dnd-test-backend
+    "^react-dnd-html5-backend$": "react-dnd-test-backend/dist/cjs",
+    "^react-dnd-test-utils$": "react-dnd-test-utils/dist/cjs"
   },
   setupFilesAfterEnv: ['./src/tests.ts'],
   snapshotSerializers: ['enzyme-to-json/serializer'],
