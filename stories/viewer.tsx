50b8ac0328635b681d47efb766f584252e83454a
@CAYdenberg <ydenberg@gmail.com>
2020-01-10 13:48:59 -0700

    Add story for viewer

--- stories/viewer.tsx
+++ stories/viewer.tsx
@@ -1,9 +1,53 @@
-import { action } from '@storybook/addon-actions'
-import { storiesOf } from '@storybook/react'
-import React from 'react'
+/*!
+ * © 2019 Atypon Systems LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-storiesOf('button', module).add('basic', () => (
-  <button type="button" onClick={action('click')}>
-    Click me
-  </button>
+import { storiesOf } from '@storybook/react'
+import { createBrowserHistory } from 'history'
+import React from 'react'
+import {
+  doc,
+  getModel,
+  manuscript,
+  modelMap,
+  project,
+  user,
+} from '../docs/lib/data'
+import { renderReactComponent, unmountReactComponent } from '../docs/lib/render'
+import { Viewer } from '../src/components/Viewer'
+import { PopperManager } from '../src/lib/popper'
+import '../styles/Editor.css'
+
+storiesOf('Viewer', module).add('basic', () => (
+  <Viewer
+    allAttachments={async () => []}
+    doc={doc}
+    getCurrentUser={() => user}
+    getManuscript={() => manuscript}
+    getModel={getModel}
+    getLibraryItem={getModel}
+    history={createBrowserHistory()}
+    locale={'en-US'}
+    manuscript={manuscript}
+    modelMap={modelMap}
+    popper={new PopperManager()}
+    projectID={project._id}
+    renderReactComponent={renderReactComponent}
+    unmountReactComponent={unmountReactComponent}
+    components={{
+      CitationViewer: () => null,
+    }}
+  />
 ))
