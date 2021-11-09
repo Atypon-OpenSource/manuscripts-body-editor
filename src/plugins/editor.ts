51afcb7ea82bdf99344f913dab2807bbaa57f8c1
Teemu Koivisto <tkoivisto456@gmail.com>
2021-11-09 10:36:55 +0200

    Remove pm-track-changes and pass it as a editor prop

--- src/plugins/editor.ts
+++ src/plugins/editor.ts
@@ -30,7 +30,6 @@ import { dropCursor } from 'prosemirror-dropcursor'
 import { history } from 'prosemirror-history'
 import { Plugin } from 'prosemirror-state'
 import { tableEditing } from 'prosemirror-tables'
-import { trackChangesPlugin } from '@manuscripts/pm-track-changes'
 
 import keys from '../keys'
 import rules from '../rules'
@@ -98,7 +97,6 @@ export default (props: PluginProps) => {
     placeholder(),
     tableEditing(),
     highlights({ setCommentTarget }),
-    trackChangesPlugin({ user: undefined }),
   ]
 }
 
