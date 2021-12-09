fc5a1ad04ea245e40cd16499f7203993bd97b9ad
Teemu Koivisto <tkoivisto456@gmail.com>
2021-12-09 15:02:47 +0200

    fix(useEditorV2) recreate state when initialDoc changes

--- src/useEditorV2.ts
+++ src/useEditorV2.ts
@@ -17,7 +17,7 @@
 import { schema } from '@manuscripts/manuscript-transform'
 import { EditorState, Transaction } from 'prosemirror-state'
 import { EditorView } from 'prosemirror-view'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 
 import { EditorProviders } from './context'
 import { useSSRLayoutEffect } from './hooks'
@@ -31,31 +31,30 @@ const useEditor = (
   editorDOMRef: React.RefObject<HTMLElement>
 ) => {
   const editorViewRef = useRef<EditorView | null>(null)
+  const [oldEditorProps, setOldEditorProps] = useState<UseEditorProps>()
 
   useSSRLayoutEffect(() => {
     if (editorDOMRef.current) {
       editorViewRef.current = init(
         editorDOMRef.current,
         editorProps,
-        editorViewRef.current
+        editorViewRef.current,
+        oldEditorProps
       )
+      setOldEditorProps(editorProps)
     }
   }, [editorDOMRef.current, editorProps])
 
   function init(
     element: HTMLElement,
     props: UseEditorProps,
-    oldView?: EditorView | null
+    oldView?: EditorView | null,
+    oldProps?: UseEditorProps
   ) {
     const { ctx } = props
     ctx.extensionProvider.init(ctx, props.extensions || [])
     if (oldView) {
-      const state = oldView.state.reconfigure({
-        plugins: [
-          ...createPlugins(props.manuscriptsProps),
-          ...ctx.extensionProvider.plugins,
-        ],
-      })
+      const state = createEditorState(ctx, props, oldView.state, oldProps)
       oldView.setProps({
         state,
         dispatchTransaction(tr: Transaction) {
@@ -84,7 +83,20 @@ const useEditor = (
     return view
   }
 
-  function createEditorState(ctx: EditorProviders, props: UseEditorProps) {
+  function createEditorState(
+    ctx: EditorProviders,
+    props: UseEditorProps,
+    oldState?: EditorState,
+    oldProps?: UseEditorProps
+  ) {
+    if (oldState && oldProps?.initialDoc === props.initialDoc) {
+      return oldState.reconfigure({
+        plugins: [
+          ...createPlugins(props.manuscriptsProps),
+          ...ctx.extensionProvider.plugins,
+        ],
+      })
+    }
     return EditorState.create({
       schema,
       doc: props.initialDoc ? schema.nodeFromJSON(props.initialDoc) : undefined,
