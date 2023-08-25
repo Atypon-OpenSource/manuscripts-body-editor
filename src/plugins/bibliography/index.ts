a8bb2c15709246a857e5b89d97518665f902b737
Jiri Kaiser <jkaiser@atypon.com>
2023-08-25 09:28:39 +0200

    cleanup

--- src/plugins/bibliography/index.ts
+++ src/plugins/bibliography/index.ts
@@ -36,7 +36,6 @@ export const bibliographyKey = new PluginKey('bibliography')
 export default (props: BibliographyProps) => {
   const getBibliographyItem = getBibliographyItemFn(props)
   const { style, locale } = props.cslProps
-  let citationsInitialised = false
 
   // if (!style) {
   //   throw new Error(`CSL Style not found`)
@@ -62,33 +61,6 @@ export default (props: BibliographyProps) => {
         const citations = buildCitations(citationNodes, (id: string) =>
           getBibliographyItem(id)
         )
-        if (!citationsInitialised) {
-          try {
-            citationsInitialised = true
-            const generatedCitations = CitationProvider.rebuildProcessorState(
-              citations,
-              getBibliographyItems(),
-              style || '',
-              locale,
-              'html'
-            ).map((item) => item[2]) // id, noteIndex, output
-
-            citationNodes.forEach(([node, pos], index) => {
-              let contents = generatedCitations[index]
-
-              if (contents === '[NO_PRINTED_FORM]') {
-                contents = ''
-              }
-
-              instance.tr.setNodeMarkup(pos, undefined, {
-                ...node.attrs,
-                contents,
-              })
-            })
-          } catch (error) {
-            console.error(error) // tslint:disable-line:no-console
-          }
-        }
 
         return {
           citationNodes,
