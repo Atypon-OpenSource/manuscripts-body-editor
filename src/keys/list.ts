933eb922cf2a2a6e5abee1767bc3d411bdf47c3c
nghazzawi-atypon <nghazzawi@atypon.com>
2023-09-04 17:43:20 +0300

    review changes

--- src/keys/list.ts
+++ src/keys/list.ts
@@ -30,7 +30,7 @@ import { EditorAction } from '../types'
 
 // TODO:: remove this command when quarterback start supporting list_item and the operation on the list
 
-const ignoreTrackChanges =
+const skipTrackingChanges =
   (command: Command) => (state: ManuscriptEditorState, dispatch?: Dispatch) => {
     command(state, (tr) => {
       if (dispatch) {
@@ -40,7 +40,8 @@ const ignoreTrackChanges =
     })
     return true
   }
-const contraolLiftListItem = (itemType: NodeType): Command => {
+// Lift the list item if it's inside a parent list.
+const liftToOuterList = (itemType: NodeType): Command => {
   return function (state: EditorState, dispatch?: (tr: Transaction) => void) {
     const { $from, $to } = state.selection
     const range = $from.blockRange(
@@ -65,12 +66,12 @@ const contraolLiftListItem = (itemType: NodeType): Command => {
 
 const listKeymap: { [key: string]: EditorAction } = {
   Enter: splitListItem(schema.nodes.list_item),
-  'Mod-[': liftListItem(schema.nodes.list_item),
-  'Mod-]': sinkListItem(schema.nodes.list_item),
+  'Mod-[': skipTrackingChanges(liftToOuterList(schema.nodes.list_item)), // outdent
+  'Mod-]': skipTrackingChanges(sinkListItem(schema.nodes.list_item)), // indent
   'Mod-Alt-o': wrapInList(schema.nodes.ordered_list),
   'Mod-Alt-k': wrapInList(schema.nodes.bullet_list),
-  'Shift-Tab': ignoreTrackChanges(contraolLiftListItem(schema.nodes.list_item)),
-  Tab: ignoreTrackChanges(sinkListItem(schema.nodes.list_item)), // indent, same as Mod-]
+  'Shift-Tab': skipTrackingChanges(liftToOuterList(schema.nodes.list_item)), // outdent, same as Mod-[
+  Tab: skipTrackingChanges(sinkListItem(schema.nodes.list_item)), // indent, same as Mod-]
 }
 
 export default listKeymap
