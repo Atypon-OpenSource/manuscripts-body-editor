ecfcd2186de70697e66a3a4552d819946fa821fc
Noura Ghazzawi <nghazzawi@atypon.com>
2023-11-22 14:45:43 +0300

    remove skip

--- src/lib/__tests__/plugins.test.ts
+++ src/lib/__tests__/plugins.test.ts
@@ -394,7 +394,7 @@ describe('editor view', () => {
     window.document.body.appendChild(root)
   })
 
-  test.skip('loads without plugins', () => {
+  test('loads without plugins', () => {
     const decoder = new Decoder(modelMap)
     const doc = decoder.createArticleNode()
 
@@ -422,7 +422,7 @@ describe('editor view', () => {
     expect(sectionWithList.childCount).toBe(2)
   })
 
-  test.skip('loads with plugins', () => {
+  test('loads with plugins', () => {
     const decoder = new Decoder(modelMap)
     const doc = decoder.createArticleNode() as ActualManuscriptNode
 
