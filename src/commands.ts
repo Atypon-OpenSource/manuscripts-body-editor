6267501b8d1365f07c5d9b4461385e33b314e53f
Ahmad Souqi <45534101+asouqi@users.noreply.github.com>
2024-05-12 20:10:25 +0300

    Incorrect Inline Subscript Numbering After Rejecting Previous Insertion and Inserting Cited Note (LEAN-3553) (#254)
    Co-authored-by: atyponci <ci@atypon.com>
    Co-authored-by: zax-assistant[bot] <103364958+zax-assistant[bot]@users.noreply.github.com>

--- src/commands.ts
+++ src/commands.ts
@@ -1173,8 +1173,9 @@ export const insertTableFootnote = (
       schema.nodes.inline_footnote
     )
     footnoteIndex =
-      inlineFootnotes.filter(({ pos }) => position + pos <= insertedAt).length +
-      1
+      inlineFootnotes.filter(
+        ({ pos }) => !isRejectedInsert(node) && position + pos <= insertedAt
+      ).length + 1
     const inlineFootnoteNode = state.schema.nodes.inline_footnote.create({
       rids: [footnote.attrs.id],
       contents: footnoteIndex === -1 ? inlineFootnotes.length : footnoteIndex,
