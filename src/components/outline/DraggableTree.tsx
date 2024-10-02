0369fc23dfa7e0b215de3af522da4eb724f38484
asouqi <asouqi@atypon.com>
2024-10-02 15:16:21 +0300

    review update

--- src/components/outline/DraggableTree.tsx
+++ src/components/outline/DraggableTree.tsx
@@ -185,13 +185,14 @@ export const DraggableTree: React.FC<DraggableTreeProps> = ({
   const [{ isOver }, dropRef] = useDrop({
     accept: 'outline',
     canDrop(item: TreeItem, monitor) {
-      if (isAbstractOrBackmatter(item) || isAbstractOrBackmatter(tree)) {
+      if (
+        !tree.parent ||
+        isAbstractOrBackmatter(item) ||
+        isAbstractOrBackmatter(tree)
+      ) {
         return false
       }
       if (!ref.current) {
-        return false
-      }
-      if (!tree.parent) {
         return false
       }
       // can't drop on itself
