8abe78aa5c4bdc593b22367d624846b22836415d
Fahid Alwaisi <82392169+fwaisi@users.noreply.github.com>
2024-10-09 16:43:10 +0300

    LEAN-3933 LW-FY25-Q2-S2: No default category is set for the sub-section

--- src/plugins/section-category/section-category-utils.ts
+++ src/plugins/section-category/section-category-utils.ts
@@ -17,7 +17,6 @@ import { SectionCategory } from '@manuscripts/json-schema'
 import { SectionCategoryIcon } from '@manuscripts/style-guide'
 import { isSectionNode, schema, SectionNode } from '@manuscripts/transform'
 import { EditorState, PluginKey } from 'prosemirror-state'
-import { findParentNodeOfTypeClosestToPos } from 'prosemirror-utils'
 import { Decoration, DecorationSet, EditorView } from 'prosemirror-view'
 import { createElement } from 'react'
 import { renderToStaticMarkup } from 'react-dom/server'
@@ -28,6 +27,7 @@ import {
   getCategoryName,
   isBackMatterSection,
   isEditableSectionCategoryID,
+  isSubSection,
   isUnique,
 } from '../../lib/section-categories'
 import { isChildOfNodeTypes } from '../../lib/utils'
@@ -164,14 +164,10 @@ export function buildPluginState(
   state.doc.descendants((node, pos) => {
     if (isSectionNode(node)) {
       const attrs = node.attrs as SectionNode['attrs']
-      const parent = findParentNodeOfTypeClosestToPos(
-        state.doc.resolve(pos),
-        schema.nodes.section
-      )
       if (
         isEditableSectionCategoryID(attrs.category as string) &&
         !isUnique(attrs.category as string) &&
-        parent?.node.type !== schema.nodes.section
+        !isSubSection(attrs.category as string)
       ) {
         decorations.push(
           Decoration.widget(pos + 1, (view) =>
