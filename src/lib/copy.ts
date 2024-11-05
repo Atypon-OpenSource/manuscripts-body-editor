3612b7842e7e20529cb3d4561ff6da3986e818a1
Fahid Alwaisi <82392169+fwaisi@users.noreply.github.com>
2024-11-05 12:50:46 +0300

    LEAN-3978 LW-FY25-Q2-S3: Table_element delete suggestion is not showing …

--- src/lib/copy.ts
+++ src/lib/copy.ts
@@ -20,37 +20,68 @@ import { EditorView } from 'prosemirror-view'
 
 import { handleTableSlice, updateSliceWithFullTableContent } from './table'
 
+
+// TODO: checking the ability to remove the following logic, after the following ticket LEAN-4118 is resolved
 export const transformCopied = (slice: Slice, view: EditorView): Slice => {
   const { state } = view
   const cutDepth = Math.min(slice.openStart, slice.openEnd)
-  if (
-    (!view.props.handleKeyDown || !state.selection.empty) &&
-    slice.content.firstChild?.type === schema.nodes.table
-  ) {
-    // Find the table_element node that contains the copied content
-    const tableElement = findParentNodeOfType(schema.nodes.table_element)(
-      state.selection
-    )
-    if (tableElement) {
-      return handleTableSlice(view, cutDepth, tableElement)
-    }
+  const tableSlice = handleTableSelection(slice, view, cutDepth)
+  if (tableSlice) {
+    return tableSlice
   }
   if (
-    slice.openStart !== slice.openEnd &&
-    (slice.content.firstChild?.type.isBlock ||
-      slice.content.lastChild?.type.isBlock)
+    slice.content.firstChild?.type.isBlock ||
+    slice.content.lastChild?.type.isBlock
   ) {
     let newSliceContent: ManuscriptNode[] = []
     slice.content.firstChild?.descendants((node) => {
-      if (node.childCount > 1) {
+      if (
+        node.childCount > 1 ||
+        (node.content.firstChild?.type === schema.nodes.table_element &&
+          node.content.lastChild?.type === schema.nodes.table_element)
+      ) {
         newSliceContent = updateSliceWithFullTableContent(state, node.content)
         return false
       }
       return true
     })
-    if (newSliceContent.length > 0) {
+    if (newSliceContent.length > 1) {
       return new Slice(Fragment.from(newSliceContent), 0, 0)
+    } else {
+      const updateSelection = handleTableSelection(
+        new Slice(
+          Fragment.from(newSliceContent),
+          slice.openStart,
+          slice.openEnd
+        ),
+        view,
+        0
+      )
+      if (updateSelection) {
+        return updateSelection
+      }
     }
   }
   return slice
+}
+
+const handleTableSelection = (
+  slice: Slice,
+  view: EditorView,
+  cutDepth: number
+): Slice | null => {
+  if (
+    (!view.props.handleKeyDown || !view.state.selection.empty) &&
+    (slice.content.firstChild?.type === schema.nodes.table ||
+      slice.content.firstChild?.type === schema.nodes.table_element)
+  ) {
+    // Find the table_element node that contains the copied content
+    const tableElement = findParentNodeOfType(schema.nodes.table_element)(
+      view.state.selection
+    )
+    if (tableElement) {
+      return handleTableSlice(view, cutDepth, tableElement)
+    }
+  }
+  return null
 }
