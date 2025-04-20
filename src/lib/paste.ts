c8e64b5a9a1270c18e01a12274f420ce31acca02
asouqi <asouqi@atypon.com>
2025-04-20 08:29:48 +0300

    Pasted Links Are Not Recognized as Hyperlinks (LEAN-4345)

--- src/lib/paste.ts
+++ src/lib/paste.ts
@@ -56,12 +56,21 @@ const wrapInSection = (slice: ManuscriptSlice) => {
   }
 }
 
+const closeAtomSlice = (slice: ManuscriptSlice) => {
+  // close slice to prevent drop of node https://github.com/ProseMirror/prosemirror-transform/blob/137ff74738bd1b50d49416cd6cfdbbf52cb059ef/src/replace.ts#L231
+  if (slice.content.firstChild?.isAtom) {
+    slice = new Slice(slice.content, 0 , 0)
+  }
+}
+
 export const transformPasted = (slice: ManuscriptSlice): ManuscriptSlice => {
   wrapInSection(slice)
 
   removeFirstParagraphIfEmpty(slice)
 
   removeIDs(slice)
+
+  closeAtomSlice(slice)
 
   return slice
 }
