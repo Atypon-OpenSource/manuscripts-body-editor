de58b3fbbf9527c4816dc1530bd8890d9ad14f6e
mbartenev-atypon <mbartenev@atypon.com>
2025-04-25 20:03:13 +0200

    updates

--- src/plugins/alt-titles.ts
+++ src/plugins/alt-titles.ts
@@ -123,7 +123,7 @@ export default () => {
     },
     appendTransaction: (transactions, _, newState) => {
       // in appendTransaction we check if alt_titles nodes exist before opening them for the first time because they are optional
-      let tr = newState.tr
+      const tr = newState.tr
       if (
         !transactions.some((tr) => tr.getMeta(altTitlesKey)) ||
         !altTitlesKey.getState(newState)
