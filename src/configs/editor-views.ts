a06dae79029ae09cb3097bcd72987e8ca6fcb093
Mohammad Betamony <mbetamony@atypon.com>
2025-05-29 12:02:32 +0300

    updates

--- src/configs/editor-views.ts
+++ src/configs/editor-views.ts
@@ -112,5 +112,6 @@ export default (
     long_desc: accessibilityElement(props, dispatch),
     alt_text: accessibilityElement(props, dispatch),
     hero_image: empty('hero_image'),
+    trans_abstract: empty('trans_abstract'),
   }
 }
