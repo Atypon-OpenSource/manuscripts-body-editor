e9da5ecb3957429ff820ef6d12facdd4db2c7cc4
mbartenev-atypon <mbartenev@atypon.com>
2025-06-11 17:28:57 +0200

    removed credit weird naming

--- src/components/authors/CreditDrawer.tsx
+++ src/components/authors/CreditDrawer.tsx
@@ -25,7 +25,7 @@ import styled from 'styled-components'
 
 import { CheckboxContainer, LabelText } from './AuthorDetailsForm'
 
-interface CRediTDrawerProps {
+interface CreditDrawerProps {
   items: { id: string; vocabTerm: string }[]
   selectedItems: {
     id: string
@@ -33,8 +33,8 @@ interface CRediTDrawerProps {
   onSelect: (id: string) => void
 }
 
-export const CRediTDrawer: React.FC<
-  CRediTDrawerProps & Omit<DrawerProps, 'children'>
+export const CreditDrawer: React.FC<
+  CreditDrawerProps & Omit<DrawerProps, 'children'>
 > = ({ items, selectedItems = [], onSelect, ...drawerProps }) => {
   return (
     <Drawer {...drawerProps}>
