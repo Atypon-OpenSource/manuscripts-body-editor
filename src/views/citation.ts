e97a89e5b79ff4c82eeb994a16602b8afff40e70
Islam Momani <imomani@atypon.com>
2025-06-30 11:33:55 +0300

    Support unstructured references - LEAN-4612 (#653)
    Co-authored-by: zax-assistant[bot] <103364958+zax-assistant[bot]@users.noreply.github.com>

--- src/views/citation.ts
+++ src/views/citation.ts
@@ -49,7 +49,11 @@ export class CitationView
     const id = this.node.attrs.id
     const text = bib.renderedCitations.get(id)
     const fragment = sanitize(
-      text && text !== '[NO_PRINTED_FORM]' ? text : ' ',
+      text === '(n.d.)'
+        ? 'Missing citation data'
+        : text && text !== '[NO_PRINTED_FORM]'
+        ? text
+        : ' ',
       {
         ALLOWED_TAGS: ['i', 'b', 'span', 'sup', 'sub', '#text'],
       }
