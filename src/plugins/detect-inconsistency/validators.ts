54e738f180537674734618d8b34da22c828f3986
Fahid Alwaisi <82392169+fwaisi@users.noreply.github.com>
2026-01-13 15:36:05 +0300

    LEAN-3843

--- src/plugins/detect-inconsistency/validators.ts
+++ src/plugins/detect-inconsistency/validators.ts
@@ -226,7 +226,6 @@ const validateFootnote: NodeValidator = (node, pos, context) => {
     : undefined
 
   const unused = footnoteState?.unusedFootnoteIDs?.has(node.attrs.id)
-
   if (unused) {
     const isTableFootnote = isChildOfNodeTypes(context.props.doc, pos, [
       schema.nodes.table_element,
