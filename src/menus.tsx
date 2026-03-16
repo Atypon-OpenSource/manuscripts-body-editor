0c003d1b1fcd2b29b64f78143f7d2af34868da04
mbartenev-atypon <mbartenev@atypon.com>
2026-03-16 16:29:03 +0100

    ups

--- src/menus.tsx
+++ src/menus.tsx
@@ -75,7 +75,7 @@ export const getEditorMenus = (
     const command = insertBackmatterSection(category)
     return {
       id: `insert-${category.id}`,
-      label: category.label || category.titles[0],
+      label: category.titles[0],
       isEnabled: isCommandValid(command),
       run: doCommand(command),
     }
