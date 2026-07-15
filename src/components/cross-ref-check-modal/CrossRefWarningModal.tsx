9c2ef6e8b69d2743f69409857af14390608764c9
m-bartenev <mbartenev@atypon.com>
2026-07-15 19:31:08 +0200

    wip

--- src/components/cross-ref-check-modal/CrossRefWarningModal.tsx
+++ src/components/cross-ref-check-modal/CrossRefWarningModal.tsx
@@ -19,17 +19,20 @@ import React, { useState } from 'react'
 
 import { CloseButton, ModalContainer, ModalHeader, StyledModal } from '@manuscripts/style-guide'
 import { StyledModalBody } from '../form/CreateModalStyles'
+import { ResolvedPos } from 'prosemirror-model'
+import { startCase, truncate } from 'lodash'
 
 export type XrefGroup = {
   referenced: ManuscriptNode,
-  xrefs: [ManuscriptNode, number][]
+  xrefs: [ManuscriptNode, ResolvedPos][]
 }
 
 export const CrossRefWarningModal: React.FC<{
   onClose: () => void,
   xrefs: XrefGroup[],
-  onConfirm: () => void
-}> = ({ onClose, xrefs }) => {
+  onConfirm: () => void,
+  selectAndScrollTo: (node: ManuscriptNode) => void
+}> = ({ onClose, xrefs, onConfirm, selectAndScrollTo }) => {
 
   const [isOpen, setIsOpen] = useState(true)
   const handleClose = () => {
@@ -45,14 +48,41 @@ export const CrossRefWarningModal: React.FC<{
     >
       <ModalContainer data-cy="cross-reference-warning-modal">
         <ModalHeader>
-          Achtung! Achtung! Element is referenced! 
+          Delete referenced content?
           <CloseButton
             onClick={() => handleClose()}
             data-cy="modal-close-button"
           />
         </ModalHeader>
-        <StyledModalBody></StyledModalBody>
+        <StyledModalBody>
+          <p>
+            You are deleting content referenced elsewhere in the document:
+          </p>
+          {
+            xrefs.map((group, i) => <XrefGroupDisplay key={i} group={group} selectAndScrollTo={selectAndScrollTo} />)
+          }
+          <div>
+            <button type='button' onClick={() => handleClose()}>Cancel</button>
+            <button type='button' onClick={() => onConfirm()}>Delete & remove citation</button>
+          </div>
+        </StyledModalBody>
       </ModalContainer>
     </StyledModal>
   )
+}
+
+const XrefGroupDisplay: React.FC<{group: XrefGroup, selectAndScrollTo: (node: ManuscriptNode) => void }> = ({ group, selectAndScrollTo }) => {
+  return <div>
+    <h3>{startCase(group.referenced.type.name)}</h3>
+    <ul>
+      {group.xrefs.map(([node, pos], i) => {
+        return (
+          <li key={i}>
+              <button onClick={() => selectAndScrollTo(node)}>
+                {`${startCase(pos.parent.type.name)} - ${truncate(pos.parent.textContent, { length: 20 })}`}
+              </button>
+          </li>)
+      })}
+    </ul>
+  </div>
 }
