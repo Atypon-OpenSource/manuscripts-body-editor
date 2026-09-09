971358e9e2577e52f450ca18f102b830521f8532
Fahid Alwaisi <82392169+fwaisi@users.noreply.github.com>
2026-09-09 15:41:33 +0300

    LEAN-6050

--- src/commands.ts
+++ src/commands.ts
@@ -1942,6 +1942,13 @@ export const addNodeComment = (
   return false
 }
 
+const canInsertHighlightMarker = ($pos: ResolvedPos) =>
+  $pos.parent.canReplaceWith(
+    $pos.index(),
+    $pos.index(),
+    schema.nodes.highlight_marker
+  )
+
 export const addInlineComment = (
   state: ManuscriptEditorState,
   dispatch?: Dispatch
@@ -1951,6 +1958,15 @@ export const addInlineComment = (
   if (!node) {
     return false
   }
+
+  if (!canInsertHighlightMarker(selection.$from)) {
+    return addNodeComment(
+      isNodeSelection(selection) ? selection.node : node,
+      state,
+      dispatch
+    )
+  }
+
   let from = selection.from
   let to = selection.to
 
